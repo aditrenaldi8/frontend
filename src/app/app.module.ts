7ce7aace3c923cba994d7c1368d946cd5e408a6d
Aditya Renaldi <aditya.renaldi@btpn.com>
2019-01-18 18:45:03 +0700

    update menu and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+// import { MatToolbarModule, MatProgressBarModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    ReactiveFormsModule,
+    // MatToolbarModule,
+    // MatProgressBarModule,
+    // MatSidenavModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
